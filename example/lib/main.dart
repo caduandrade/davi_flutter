095a9387876d621bb3e9c66647fd877a6654097e
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2022-12-20 23:12:53 -0300

    Updating the main example. #35

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:easy_table/easy_table.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +8,23 @@ void main() {
 }
 
 class Person {
-  Person(this.name, this.age);
+  Person(this.name, this.value);
 
   final String name;
-  final int age;
+  final int value;
+
+  bool _valid = true;
+
+  bool get valid => _valid;
+
+  String _editable = '';
+
+  String get editable => _editable;
+
+  set editable(String value) {
+    _editable = value;
+    _valid = _editable.length < 6;
+  }
 }
 
 class ExampleApp extends StatelessWidget {
@@ -39,19 +54,40 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    List<Person> rows = [
-      Person('Landon', 19),
-      Person('Sari', 22),
-      Person('Julian', 37),
-      Person('Carey', 39),
-      Person('Cadu', 43),
-      Person('Delmar', 72)
-    ];
+    List<Person> rows = [];
+
+    Random random = Random();
+    for (int i = 1; i < 500; i++) {
+      rows.add(Person('User $i', random.nextInt(100)));
+    }
 
     _model = EasyTableModel<Person>(rows: rows, columns: [
       EasyTableColumn(name: 'Name', stringValue: (row) => row.name),
-      EasyTableColumn(name: 'Age', intValue: (row) => row.age)
+      EasyTableColumn(name: 'Value', intValue: (row) => row.value),
+      EasyTableColumn(
+          name: 'Editable',
+          cellBuilder: _buildField,
+          cellBackground: (rowData) =>
+              rowData.row.valid ? Colors.transparent : Colors.red[800]),
     ]);
+  }
+
+  Widget _buildField(BuildContext context, RowData<Person> rowData) {
+    return TextFormField(
+        initialValue: rowData.row.editable,
+        style:
+            TextStyle(color: rowData.row.valid ? Colors.black : Colors.white),
+        onChanged: (value) => _onFieldChange(value, rowData.row));
+  }
+
+  void _onFieldChange(String value, Person person) {
+    final wasValid = person.valid;
+    person.editable = value;
+    if (wasValid != person.valid) {
+      setState(() {
+        // rebuild
+      });
+    }
   }
 
   @override
