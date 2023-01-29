bfd5d670c806696d8bff1ee5001fded95ea7bf53
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2023-01-29 16:44:13 -0300

    Set the direction parameter of DaviSort as optional. #67

--- lib/src/sort.dart
+++ lib/src/sort.dart
@@ -2,7 +2,7 @@ import 'package:davi/src/sort_direction.dart';
 
 /// Describes the [Davi] sort.
 class DaviSort {
-  DaviSort(this.id, this.direction);
+  DaviSort(this.id, [this.direction = DaviSortDirection.ascending]);
 
   final dynamic id;
   final DaviSortDirection direction;
