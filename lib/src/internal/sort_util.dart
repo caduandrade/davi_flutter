cd9d0731bfb3ddc3d320703d3063e2d939970b2b
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2023-01-31 17:21:57 -0300

    Removing trash code. #67

--- lib/src/internal/sort_util.dart
+++ lib/src/internal/sort_util.dart
@@ -13,7 +13,7 @@ class SortUtil {
       required dynamic columnIdToSort}) {
     List<DaviSort> newSortList = [];
 
-    if(multiSortEnabled) {
+    if (multiSortEnabled) {
       bool needAddColumn = true;
       for (int index = 0; index < sortList.length; index++) {
         DaviSort sort = sortList[index];
@@ -21,58 +21,25 @@ class SortUtil {
           needAddColumn = false;
           if (index == sortList.length - 1) {
             if (sort.direction == DaviSortDirection.ascending) {
-              newSortList.add(
-                  DaviSort(columnIdToSort, DaviSortDirection.descending));
+              newSortList
+                  .add(DaviSort(columnIdToSort, DaviSortDirection.descending));
             }
           }
           continue;
         }
-          newSortList.add(
-              DaviSort(sort.columnId, sort.direction));
+        newSortList.add(DaviSort(sort.columnId, sort.direction));
       }
       if (needAddColumn || (alwaysSorted && newSortList.isEmpty)) {
         newSortList.add(DaviSort(columnIdToSort, DaviSortDirection.ascending));
       }
     } else {
-      DaviSort? sort = sortList.firstWhereOrNull((sort)=>sort.columnId==columnIdToSort);
+      DaviSort? sort =
+          sortList.firstWhereOrNull((sort) => sort.columnId == columnIdToSort);
       if (sort?.direction == DaviSortDirection.ascending) {
-        newSortList.add(
-            DaviSort(columnIdToSort, DaviSortDirection.descending));
-      } else if(alwaysSorted || sort==null) {
-        newSortList.add(
-            DaviSort(columnIdToSort, DaviSortDirection.ascending));
+        newSortList.add(DaviSort(columnIdToSort, DaviSortDirection.descending));
+      } else if (alwaysSorted || sort == null) {
+        newSortList.add(DaviSort(columnIdToSort, DaviSortDirection.ascending));
       }
-    }
-    return newSortList;
-  }
-
-  /// Creates a new sort list given a list of sorted columns,
-  /// model configuration and the new column.
-  static List<DaviSort> newSortListOLD(
-      {required List<DaviSort> sortList,
-        required bool multiSortEnabled,
-        required bool alwaysSorted,
-        required dynamic columnIdToSort}) {
-    List<DaviSort> newSortList = [];
-
-    bool needAddColumn = true;
-    for (int index = 0; index < sortList.length; index++) {
-      DaviSort currentSort = sortList[index];
-      if (currentSort.columnId == columnIdToSort) {
-        needAddColumn = false;
-        if (index == sortList.length - 1) {
-          if (currentSort.direction == DaviSortDirection.ascending) {
-            newSortList.add(DaviSort(columnIdToSort, DaviSortDirection.descending));
-          }
-        }
-        continue;
-      }
-      if (multiSortEnabled) {
-        newSortList.add(DaviSort(currentSort.columnId, currentSort.direction));
-      }
-    }
-    if (needAddColumn || (alwaysSorted && newSortList.isEmpty)) {
-      newSortList.add(DaviSort(columnIdToSort, DaviSortDirection.ascending));
     }
     return newSortList;
   }
