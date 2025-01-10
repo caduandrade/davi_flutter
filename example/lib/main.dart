586bdaac62bcea6fb164bd15bd161fc5d88547ed
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2025-01-10 09:03:00 -0300

    Cleaning up the main example. #97

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,12 +7,11 @@ void main() {
   runApp(const ExampleApp());
 }
 
-class Person {
-  Person(this.name, this.age, this.value, this.bar);
+class Data {
+  Data({required this.stringValue, required this.intValue,required this.bar});
 
-  final String name;
-  final int age;
-  final int? value;
+  final String stringValue;
+  final int? intValue;
 
   final double? bar;
 
@@ -53,7 +52,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late DaviModel<Person> _model;
+  late DaviModel<Data> _model;
 
   @override
   void initState() {
@@ -62,110 +61,81 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _buildModel() {
-    List<Person> rows = [];
-
     Random random = Random();
-    for (int i = 1; i < 200; i++) {
-      String name = 'User $i';
-      if (i == 4) {
-        name += ' 12345678901234567890';
-      }
-      rows.add(Person(name, 20 + random.nextInt(50), i == 1 ? null : i, random.nextDouble()));
+    List<Data> rows = [];    
+    for (int i = 1; i < 200; i++) {    
+      String stringValue= random.nextInt(90000).toRadixString(16);
+     int intValue = random.nextInt(99);
+     double bar = random.nextDouble();      
+      rows.add(Data(stringValue: stringValue, intValue: intValue, bar: bar));
     }
     // rows.shuffle();
 
-    _model = DaviModel<Person>(
+    _model = DaviModel<Data>(
         rows: rows,
         columns: [
-/*
-        DaviColumn(
-        name: 'Name',
-        rowSpan: (d,r)=>r==10?5:1,
-        cellValue: (data, rowIndex) =>data.name
-        ),*/
-
           DaviColumn(
-              name: 'Name',
-              cellValue: (data, rowIndex) =>
-                  rowIndex == 0 ? 'SPAN 1234567890123456789' : data.name,
-             //rowSpan: (data, rowIndex) => rowIndex==1?2:1,
-              columnSpan: (data, rowIndex) => rowIndex == 0 ? 2 : 1,
-              pinStatus: PinStatus.left),
-
-          DaviColumn(
-              name: 'Age',
-              cellValue: (data, rowIndex) => data.age,
-              pinStatus: PinStatus.left,
-              summary: (context) => const Text('test')),
-
-          DaviColumn(
-              name: 'Value',
-              cellValue: (data, rowIndex) => data.value,
+              name: 'String',
+              cellValue: (data, rowIndex) => data.stringValue,
               pinStatus: PinStatus.left),
           DaviColumn(
-              name: 'Painter',
-              cellPainter: _cellPainter),
+              name: 'Int 1',
+              cellValue: (data, rowIndex) => data.intValue,
+              summary: (context) => const Text('summary')),
           DaviColumn(
-              name: 'Value 3',
-              cellWidget: (w,c,i)=>i==10?Placeholder():null,
-              //cellValue: (data, rowIndex) =>  rowIndex == 4 ? 'SPAN R4C4' : data.value?.toString(),
-              rowSpan: (data, rowIndex) => rowIndex == 10 ? 6 : 1
-    ),
-
-          DaviColumn(
-              name: 'Value 4',
+              name: 'Int 2',
               cellValue: (data, rowIndex) =>
-                  rowIndex == 2 ? 'SPANNNNNNN R2C5' : data.value,
-              cellTextStyle: (data, rowIndex, hovered)=>const TextStyle(fontWeight: FontWeight.bold),
+              rowIndex == 2 ? 'SPAN' : data.intValue,
               columnSpan: (data, rowIndex) => rowIndex == 2 ?2 : 1,
               cellBackground: (data, index, hovered) =>
-                  data.value == 12 ? Colors.green : null),
+              data.intValue == 10 ? Colors.green : null),
+          DaviColumn(
+              name: 'Widget',
+              cellWidget: (context,data,rowIndex)=>rowIndex==10?Container(color:Colors.white, child:const Placeholder()):null,
+              rowSpan: (data, rowIndex) => rowIndex == 10 ? 6 : 1
+    ),
           DaviColumn(
               name: 'Bar',
-              cellBarValue: (data, rowIndex) => data.bar),
-          DaviColumn(
-              name: 'Value 6',
-              cellValue: (data, rowIndex) => data.value),
-          DaviColumn(
-              name: 'Value 7',
-              cellValue: (data, rowIndex) => data.value
-          ),
-/*
+              cellBarValue: (data, rowIndex) => data.bar),   
             DaviColumn(
               name: 'Editable',
               sortable: false,
               cellWidget: _buildField,
-              //cellBackground: (row, index, hover) => data.valid ? null : Colors.red[800]
+              cellBackground: (data, rowIndex, hover) => data.valid ? null : Colors.red[800]
             )
-*/
         ],
-      //  alwaysSorted: true,
         multiSortEnabled: true
     );
   }
 
   Widget _buildField(
-      BuildContext context, Person data, int index) {
+      BuildContext context, Data data, int index) {
     if (true) {
       return TextFormField(
+        validator: (s)=>'ds',
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           controller: data.c,
           //key: ValueKey(rowData.index),
           //initialValue: rowData.data.editable,
           onChanged: (value) => _onFieldChange(value, data));
     }
     return TextFormField(
+      key: ValueKey(index),
         initialValue: data.editable,
         style: TextStyle(color: data.valid ? Colors.black : Colors.white),
         onChanged: (value) => _onFieldChange(value, data));
   }
 
-  void _onFieldChange(String value, Person person) {
+  void _onFieldChange(String value, Data person) {
     final wasValid = person.valid;
     person.editable = value;
     if (wasValid != person.valid) {
+      /*
       setState(() {
         // rebuild
       });
+
+       */
     }
   }
 
@@ -173,54 +143,31 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     DaviTheme theme = DaviTheme(
         data: DaviThemeData(
-            columnDividerThickness: 10,
-            columnDividerColor: Colors.yellow,
-            header: const HeaderThemeData(bottomBorderThickness: 5),
             row: RowThemeData(
               fillHeight: true,
-              dividerThickness: 10,
-              dividerColor: Colors.pink,
-              color: RowThemeData.zebraColor(
-                  evenColor: Colors.pink[100], oddColor: Colors.yellow[100]),
-                //hoverBackground: (index) => Colors.blue[300],
-             // hoverForeground: (index) => Colors.blue[300]!.withOpacity(.5),
+              color: RowThemeData.zebraColor(),
+              hoverForeground: (index) => Colors.blue[300]!.withOpacity(.2),
             ),
             cell: CellThemeData(
-                nullValueColor: (index, hover) =>
-                    hover ? Colors.yellow : Colors.orange)),
-        child: Davi<Person>(
+                nullValueColor: (index, hover) => Colors.grey)),
+        child: Davi<Data>(
           _model,
           // onHover: (index) => print('hover: $index'),
-          onRowTap: _onRowTap,
-           // onLastVisibleRow: (index)=>print('last visible row: $index ${DateTime.now()}'),
-           // onTrailingWidget: (visible)=>print('trailing widget: $visible ${DateTime.now()}'),
-           trailingWidget: MouseRegion(onHover: (h)=>print('hover on trailing ${DateTime.now()}'), child: const Center(child: Text('trailing widget')))
+          onRowTap: _onRowTap
         ));
 
     return Scaffold(
-        body: Row(children: [SizedBox(width: 50),Expanded(child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      TextButton(onPressed: _onclick, child: Text('ok')),
-      Expanded(child: theme)
-    ]))]));
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      Center(child: TextButton(onPressed: _onTextButtonClick, child: const Text('rebuild'))),
+      Expanded(child: Padding(padding: const EdgeInsets.all(32), child: theme))
+    ]));
   }
 
-
-  void _onHover(int? index) {
-    print('onHover: $index');
+  void _onRowTap(Data p) {
+    print('tap: ${p.intValue}');
   }
 
-  void _onRowTap(Person p) {
-    print('tap: ${p.value}');
-  }
-
-  void _cellPainter(Canvas canvas, Size size, Person person) {
-    final Paint paint = Paint()..color = Colors.blue;
-      canvas.drawLine(Offset.zero, Offset(size.width, size.height),
-         paint,
-      );
-  }
-
-  void _onclick() {
+  void _onTextButtonClick() {
     setState(() {
       _buildModel();
     });
